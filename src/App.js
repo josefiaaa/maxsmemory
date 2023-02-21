98b8587aa07144033f900e728214f626f0a151bd
“Josefia <josefia.magagnoli@gmail.com>
2023-02-21 14:51:33 -0500

    more app setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,71 @@
+import NavBar from './NavBar/NavBar';
+import Section from './Sections/Sections';
+import CTA from './CTA/CTA';
+import About from './About/About';
+import Cards from './Cards/Cards';
+import Foundations from './Foundations/Foundations';
+import Donate from './Donate/Donate';
+// import Gallery from './Gallery/Gallery';
+
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+    return (    
+        <div className="App">
 
-      </header>
-    </div>
-  );
+            {/* HEADER */}
+            <header className="App-header">
+                <NavBar />
+            </header>
+
+            <CTA />
+
+            <div className='Main'>
+                {/* SECTIONS */}
+                {/* WHEN EDITING A SECTION, BE SURE TO UPDATE THE CORRESPONDING LINKS IN NAVBAR.JS */}
+                {/* SECTION 1 */}
+                <Section
+                    title="In Memory of Maxwell Atkins"
+                    subtitle={<About />}
+                    dark={true}
+                    id="section1"
+                />
+
+                <Cards />
+
+                {/* <Gallery /> */}
+
+                {/* SECTION 2 */}
+                <Section 
+                    title="Foundations"
+                    subtitle={<Foundations />}
+                    id='section2'
+                />
+
+                {/* SECTION 3 */}
+                <Section 
+                    title="Donate"
+                    subtitle={<Donate />}
+                    id='section3'
+                />
+
+                {/* SECTION 4 */}
+                <Section 
+                    title="Musicians"
+                    // subtitle={<Musicians />}
+                    id='section4'
+                />
+
+                { /* SECTION 5 */}
+                    <Section 
+                    title="Contributers"
+                    // subtitle={<Contributers />} 
+                    id='section5'
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
